ebf46dd830c06c5f98120e33c445beba958d5206
chris.xiao <xiaohexh@hotmail.com>
2016-08-14 00:24:20 -0700

    python_learning
    add some string operation
    
    Signed-off-by: chris.xiao <xiaohexh@hotmail.com>

--- string.py
+++ string.py
@@ -3,7 +3,25 @@
 
 str = 'addpersontoour'
 
+# substring
+print str[5]
 print str[0:4]
 print str[-3:-1]
 print str[6:]
 print str[:-2]
+
+# cat
+str += "_postfix"
+print str
+
+# repeat
+print str * 3
+
+# length
+print len(str)
+
+# empty
+if not str:
+	print "Empty string"
+else:
+	print str
